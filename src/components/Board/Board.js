3aeabfcccebafdcbb1b83c0e00e880cf9e689d73
garenv <garenvartanian24@gmail.com>
2018-10-04 12:23:19 -0400

    fixed issue with no drawn match being displayed

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -8,54 +8,84 @@ class Board extends Component {
             turn: 'X',
             gameEnded: false,
             winner: undefined,
+        };
+
+        this.gameState = {
             board: Array(9).fill(''),
             totalMoves: 0
         }
     }
 
     clicked(event) {
-        if(this.state.board[event.target.dataset.square] === '') {
-            this.state.board[event.target.dataset.square] = this.state.turn;
+        if(this.gameState.board[event.target.dataset.square] === '') {
+            this.gameState.board[event.target.dataset.square] = this.state.turn;
             event.target.innerText = this.state.turn;
 
             this.setState({
-                turn: this.state.turn === 'X' ? 'O' : 'X',
-                board: this.state.board,
-                totalMoves: this.state.totalMoves++
+                turn: this.state.turn === 'X' ? 'O' : 'X'
             });
-        }
 
+            this.gameState.totalMoves++;
+        }
         var result = this.checkWinner();
 
         if(result === 'X') {
             this.setState({
                 gameEnded: true,
-                winner: 'X'
+                winner: 'X',
+                winnerLine: 'X wins'
             });
             console.log("X wins");
         } else if(result === 'O') {
             this.setState({
                 gameEnded: true,
-                winner: 'O'
+                winner: 'O',
+                winnerLine: 'O wins'
             });
             console.log("O wins");
+        } else if(result === "draw") {
+            this.setState({
+               gameEnded: true,
+               winner: 'draw',
+               winnerLine: 'match is a draw'
+            });
         }
     }
 
     checkWinner() {
         var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
-        var board = this.state.board;
+        var board = this.gameState.board;
 
         for(let i = 0; i < moves.length; i++) {
             if(board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]]) {
                 return board[moves[i][0]];
             }
         }
+
+        console.log(this.gameState.totalMoves);
+        if(this.gameState.totalMoves === 9) {
+            return "draw";
+        }
     }
+
+    //compute rand num from 0-8
+    // computeRandomNumber() {
+    //     let randNum = Math.floor((Math.random() * 8));
+    //     return randNum;
+    // }
+    //
+    //
+    // cupTurn() {
+    //     do {
+    //      this.computeRandomNumber();
+    //     } while (this.gameState.board !== '');
+    // }
+
 
     render() {
         return(
             <div id="game">
+                <div id="state">{this.state.winnerLine}</div>
                 <div id="head">
                     Tic Tac Toe
                 </div>
